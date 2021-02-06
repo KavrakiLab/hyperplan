1a3d16b53ad82c0bf5dbfcf1b284be5ba7202a9c
Mark Moll <mmoll@rice.edu>
2021-02-06 17:34:11 -0600

    fix path in tests

--- examples/robowflex.sh
+++ examples/robowflex.sh
@@ -3,26 +3,9 @@ source ${HOME}/ws_hyperplan/devel/setup.bash
 source ${HOME}/ws_hyperplan/venv/bin/activate
 cd ${HOME}/ws_hyperplan/src/hyperplan
 
-num_workers=${1:-8}
-run_id=${2:-0}
-opt=${3:-speed}
-problem=${4:-small_pick}
-#outdir=${HOME}/Bubox/archive/mark_moll/hyperplan/robowflex/${opt}/${problem}
-outdir=/tmp/hyperplan/robowflex/${opt}/${problem}
-max_budget=${5:-729}
-num_iterations=${6:-7}
-worker=${7:-}
+problem=${1:-box_pick}
+opt=${2:-speed}
+worker=${3:-}
 
-for i in `seq ${num_workers}`; do
-    mkdir -p ${outdir}
-    (nohup ./scripts/hyperplan_cmdline.py \
-    --run_id ${run_id} \
-    --n_workers ${num_workers} \
-    --shared_directory ${outdir} \
-    --n_iterations ${num_iterations} \
-    --max_budget ${max_budget} \
-    --backend robowflex \
-    --opt ${opt} \
-    ${worker} examples/fetch/${problem} >> ${outdir}/hyperplan.log) &
-    worker='--worker'
-done
+(nohup ./scripts/hyperplan_cmdline.py \
+${worker} examples/fetch/${problem}-${opt}.yaml >> /tmp/hyperplan-${problem}-${opt}.log) &
