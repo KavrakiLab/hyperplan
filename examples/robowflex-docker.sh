54c1c2a695e5eb5382bec4efa3d7e70047dbea31
Mark Moll <mmoll@rice.edu>
2021-05-28 17:10:13 -0500

    add example script for running HyperPlan inside docker container, update Dockerfile, simplify command line scripts

---
+++ examples/robowflex-docker.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+robot=${1:-fetch}
+problem=${2:-box_pick}
+opt=${3:-speed}
+testing=${4:-}
+name=${problem}-${opt}
+if [ "$testing" == "--test" ]; then
+    suffix="-test"
+fi
+
+docker run \
+   --name ${name}${suffix} \
+   --mount type=bind,source=${HOME}/Bubox/archive/mark_moll,target=/ws_hyperplan/data \
+   hyperplan ${testing} \
+   /ws_hyperplan/data/hyperplan-fetch/${problem}-${opt}.yaml >> /tmp/hyperplan-${problem}-${opt}.log
